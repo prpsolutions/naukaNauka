b585fc20da2aa1987a43de1aae7434885d8af24e
prpsolutions <info@prpsolutions.eu>
2019-12-19 22:10:37 +0100

    druga proba

--- src/main/java/com/kodilla/AudiMain.java
+++ src/main/java/com/kodilla/AudiMain.java
@@ -2,7 +2,7 @@ package com.kodilla;
 
 public class AudiMain {
     public static void main(String [] args){
-        Audi obiekt = new Audi("A5",500000);
+        Audi obiekt = new Audi("A5",60000);
         System.out.println(obiekt);
     }
 }
